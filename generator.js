ff97c78782afd82163aec1b4a9b6a13f66c74691
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-11 16:28:33 -0700

    updated files for readme to have response

---
+++ generator.js
@@ -0,0 +1,5 @@
+const { fstat } = require("fs");
+
+
+//  I want put the generator function in here to know what it does with the inqured information //
+
---
+++ index.js
@@ -0,0 +1,37 @@
+//dependensies
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
+// import your function generator
+
+
+
+
+//these questions are for inquirer to promt the user for input
+const questions = [
+    {
+        type:"input",
+        name:"github",
+        message:"What is your github username?"
+    },
+    {
+        type:"",
+        name:"",
+        message:""
+    }
+]
+
+//function to create readme and write this readme file 
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+
+//start the node application and handles other functions
+function start() {
+    inquirer.prompt(questions).then((inquirerResponses)=>{
+        writeToFile("README.md", generator({...inquirerResponses}))
+    })
+}
+
+start();
