c005a3e205ad80dc03ecc2ea6fd68f47ec4d58b2
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-12 14:38:32 -0700

    commented out tutor work. WAY TO ADVICE!

--- index.js
+++ index.js
@@ -21,19 +21,29 @@ const questions = [
     }
 ]
 
+
+
+
+
+
+
+
+
+//  This way was taught to me by my tutor. I could not figure it out however, I wanted to keep it to make sure that I could reference it one day // I have been recommended TO NOT USE THIS due to its difficulty. 
+
 //function to create readme and write this readme file 
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.append(process.cwd(), fileName), data);
-}
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(path.append(process.cwd(), fileName), data);
+// }
 
 
 //start the node application and handles other functions
-function start() {
-    inquirer.prompt(questions)
-    .then((inquirerResponses)=> {
-        writeToFile("README.md", generator({...inquirerResponses}))
-    })
-    .catch(err => console.log(err))
-}
+// function start() {
+//     inquirer.prompt(questions)
+//     .then((inquirerResponses)=> {
+//         writeToFile("README.md", generator({...inquirerResponses}))
+//     })
+//     .catch(err => console.log(err))
+// }
 
-start()
+// start()
