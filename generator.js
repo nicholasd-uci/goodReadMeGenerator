cdb16981ebdffe0a374fed6d595a268a74e23ef9
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-11 22:57:32 -0700

    updated files

--- generator.js
+++ generator.js
@@ -3,3 +3,14 @@ const { fstat } = require("fs");
 
 //  I want put the generator function in here to know what it does with the inqured information //
 
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+    # ${data.Title}
+    ** ${data.Description}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
--- index.js
+++ index.js
@@ -11,27 +11,29 @@ const inquirer = require('inquirer')
 const questions = [
     {
         type:"input",
-        name:"github",
-        message:"What is your github username?"
+        name:"Title",
+        message:"What would you like Title to be?"  
     },
     {
-        type:"",
-        name:"",
-        message:""
+        type:"input",
+        name:"Description",
+        message:"What is to be?"
     }
 ]
 
 //function to create readme and write this readme file 
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    return fs.writeFileSync(path.append(process.cwd(), fileName), data);
 }
 
 
 //start the node application and handles other functions
 function start() {
-    inquirer.prompt(questions).then((inquirerResponses)=>{
+    inquirer.prompt(questions)
+    .then((inquirerResponses)=> {
         writeToFile("README.md", generator({...inquirerResponses}))
     })
+    .catch(err => console.log(err))
 }
 
-start();
+start()
